74ea0fcb51d9d9f63249fbe60d1b43c01efde4c7
Joseph Sankah <125163762+Josephsankah@users.noreply.github.com>
2023-04-28 04:58:12 -0400

    Create project3.s
    These would be the data would be using in our program( under the data section). An input_str to store the data from the user, an error_msg to store the error message string, for a comma, an opening parenthesis and finally a closing parenthesis.

---
+++ project3.s
@@ -0,0 +1,7 @@
+.data
+	input_str: .space 1001			# -> label for user input
+	error_msg: .asciiz "X"				# -> For_Invalid message
+	comma_char: .asciiz ","               # -> comma string
+	opening_char: .asciiz "("                # -> opening parenthesis
+	closing_char: .asciiz ")"              # -> closing parenthesis
+
